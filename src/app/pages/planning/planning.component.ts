124939ae7d639eb0ec0367fceb8ef0b467026a55
mrcgit <mrc.ildp@gmail.com>
2024-05-14 12:17:09 +0200

    Changing planning page par

--- src/app/pages/planning/planning.component.ts
+++ src/app/pages/planning/planning.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-planning',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './planning.component.scss'
 })
 export class PlanningComponent {
+  feature: string = '';
+
+
+  constructor(private route: ActivatedRoute){
+    // To extract route param value
+    this.route.paramMap.subscribe(
+      (params)=> {
+        let feature = params.get('feature')
+        console.log(feature);
+        if(feature){
+         this.feature = feature
+        } else {
+          console.error("Errore recupero selle sezione")
+        }
+      }
+    )
+  }
 
 }
